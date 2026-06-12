8a8e67198b9b543523c8fe47ea2c91c1f4dd7c35
Supriyo95 <supriyo2001dolui@gmail.com>
2026-06-12 11:31:20 +0530

    main.py file change

--- main.py
+++ main.py
@@ -84,9 +84,9 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        "app.main:app",
-        host=settings.app_host,
-        port=settings.app_port,
-        reload=settings.debug,
-        log_level="info",
-    )
+    app,
+    host=settings.app_host,
+    port=settings.app_port,
+    reload=settings.debug,
+    log_level="info",
+)
