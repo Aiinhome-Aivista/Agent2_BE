065a24cdd4ae633b7e265b4996fef3f4b0a733f0
Supriyo95 <supriyo2001dolui@gmail.com>
2026-07-31 11:58:19 +0530

    cors fix

--- main.py
+++ main.py
@@ -56,6 +56,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.cors_origins_list,
+    allow_origin_regex=".*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
