b7bf278eed176c071ae75a2df26657691621caad
Supriyo95 <supriyo2001dolui@gmail.com>
2026-08-10 09:32:53 +0530

    email connector implementation

--- test_assign.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('d:\\aiInHomes_workplace\\Agent_2\\backend')
-from app.db.database import get_db
-
-with get_db() as conn:
-    with conn.cursor(dictionary=True) as cur:
-        cur.execute('SELECT u.id, u.full_name, u.role, (SELECT COUNT(*) FROM incidents i WHERE i.assigned_to = u.id) as ticket_count FROM users u WHERE u.is_active = 1')
-        print(cur.fetchall())
